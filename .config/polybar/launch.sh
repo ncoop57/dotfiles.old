7c25d0d258794372e175682b637948bc61d860ec
ncoop57 <nac33@students.uwf.edu>
2019-06-07 08:32:42 -0500

    Updated i3 and polybar config to support multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 #while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+# polybar main &
 
 echo "Bars launched..."
