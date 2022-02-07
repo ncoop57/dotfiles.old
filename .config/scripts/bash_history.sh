dd7485a694ff074e801f6a8c7df227ab77a1d043
ncoop57 <nacooper01@email.wm.edu>
2022-02-06 21:13:43 -0500

    starting fresh

--- .config/scripts/bash_history.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-HISTFILE=~/.bash_history
-set -o history
-
-#eval $(history | awk '{$1=""; print $0}' | rofi -dmenu -i -p "Bash History" -config ~/.config/rofi/history.rasi)
-history | awk '{$1=""; print $0}' | rofi -dmenu -i -p "Bash History" -config ~/.config/rofi/history.rasi | xclip -sel clip
--- .config/scripts/terminal_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -- bash -c "man man; exec bash"
-gnome-terminal -- bash -c "neofetch; exec bash"
-gnome-terminal -- bash -c "htop; exec bash"
